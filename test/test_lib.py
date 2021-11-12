c85d21e7f5fc74a0009df5e1f6263cbb854e6b57
Rodrigo <31988896+ro-56@users.noreply.github.com>
2021-11-11 21:24:03 -0300

    test: testing actions

--- test/test_lib.py
+++ test/test_lib.py
@@ -4,7 +4,7 @@ def test_get_config():
     config = lib.get_config('docs/test.yaml')
     expected = {
         'email': 'email@mail.com',
-        'workspace_id': '123456789',
+        'workspace_id': 123456789,
         'api_token': 'LoremIpsumDolorSitAmetConsecteturAdipiscingElit',
         'sgu_username': 'LoremIpsum',
         'output_file_name': 'output.csv'
