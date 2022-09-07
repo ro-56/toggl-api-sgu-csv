80cf10d8eaf08cf4b6ab5c00252bfb7b3455a829
Rodrigo <31988896+ro-56@users.noreply.github.com>
2022-09-06 22:33:37 -0300

    fix: ajusta comando quando utilizando --help pela primeira vez

--- src/togglreports/core/app.py
+++ src/togglreports/core/app.py
@@ -18,14 +18,19 @@ class Application():
         cli_parser.process_arguments(self._parser)
 
     def _initialize(self):
-        # Initialize the configuration if not present
-        if not config.config_exists():
-            config.init_config()
-
+        # Initialize parser
         self._parser = cli_parser.create_parser()
         if not self._parser:
             raise Exception('Parser not initialized')
-            exit(1)
+        
+        # Check parser for the --help command
+        # Prints the help section and exits if --help was used
+        self._parser.parse_args()
+
+        # Initialize the configuration if it's not present
+        if not config.config_exists():
+            config.init_config()
+
 
     def _load_plugins(self):
         plugin_loader.load_plugins()
